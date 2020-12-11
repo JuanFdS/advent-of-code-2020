40d4d39c5303696d021b906e18ff4f35b097cb57
lautaro <lautaro.garcia@10pines.com>
2020-12-11 18:34:10 -0300

    WIP

--- dia-1/reportRepair.lisp
+++ dia-1/reportRepair.lisp
@@ -1,4 +1,15 @@
+(defparameter *lista-de-prueba* '(1721 979 366 299 675 1456))
+
+(defun obtener-input (path)
+  (with-open-file (archivo path)
+    (read-line archivo)
+    (read-line archivo)))
+
 (defun complemento (expensa) (- 2020 expensa))
-(defun tenemosSuComplemento (expensa expensas) (find (complemento expensa) expensas))
+
+(defun tenemosSuComplemento (expensa expensas)
+  (find (complemento expensa) expensas))
+
 (defun solucionarProblema (expensas)
-    (find-if (lambda (expensa) (tenemosSuComplemento expensa expensas)) expensas))
+  (let ((expensa-buscada (find-if (lambda (expensa) (tenemosSuComplemento expensa expensas)) expensas)))
+    (* expensa-buscada (complemento expensa-buscada))))
