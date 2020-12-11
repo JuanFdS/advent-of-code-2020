aea27a4c52469e61dfb73f009e84c02006d3fe63
lautaro <lautaro.garcia@10pines.com>
2020-12-11 19:01:16 -0300

    Solución 1

--- dia-1/reportRepair.lisp
+++ dia-1/reportRepair.lisp
@@ -1,16 +1,16 @@
 (defparameter *lista* '())
 
 (defun obtener-input (path)
-  (with-open-file (archivo path)
-    (print (read-line archivo))
-    (loop :for linea = (read-line archivo nil))
-          :do (print linea)))
+  (mapcar #'parse-integer (uiop:read-file-lines path)))
 
 (defun complemento (expensa) (- 2020 expensa))
 
 (defun tenemosSuComplemento (expensa expensas)
   (find (complemento expensa) expensas))
 
-(defun solucionarProblema (expensas)
+(defun producto-de-expensas-buscadas (expensas)
   (let ((expensa-buscada (find-if (lambda (expensa) (tenemosSuComplemento expensa expensas)) expensas)))
     (* expensa-buscada (complemento expensa-buscada))))
+
+(defun solucionarProblema (path)
+  (producto-de-expensas-buscadas (obtener-input path)))
