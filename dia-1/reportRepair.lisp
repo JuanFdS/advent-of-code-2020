4591579d1dee01a266bd72cf7422c8b516c08eb7
juanFdS <juan.fernandes@10pines.com>
2020-12-11 19:56:57 -0300

    Esta andando :D

--- dia-1/reportRepair.lisp
+++ dia-1/reportRepair.lisp
@@ -1,12 +1,8 @@
 (defparameter *lista* (obtener-input "dia-1/input.txt"))
+(defparameter *anioDelMal* 2020)
 
 (defun obtener-input (path)
   (mapcar #'parse-integer (uiop:read-file-lines path)))
-
-(defun complemento (expensa) (- 2020 expensa))
-
-(defun tenemosSuComplemento (expensa expensas)
-  (find (complemento expensa) expensas))
 
 (defun tenemosSuComplementoDe (expensa total expensas)
   (find (- total expensa) expensas))
@@ -22,9 +18,11 @@
       (multiple-value-bind (uno dos) (encontrar-dos-que-sumen (- total primer-sumando) valores)
         (values uno dos primer-sumando)))))
 
-(defun producto-de-expensas-buscadas (expensas)
-  (let ((expensa-buscada (find-if (lambda (expensa) (tenemosSuComplemento expensa expensas)) expensas)))
-    (* expensa-buscada (complemento expensa-buscada))))
+(defmacro product (form)
+    `(multiple-value-call #'* ,form))
 
-(defun solucionarProblema (path)
-  (producto-de-expensas-buscadas (obtener-input path)))
+(defun solucionarProblema1 (path)
+  (product (encontrar-dos-que-sumen *anioDelMal* (obtener-input path))))
+
+(defun solucionarProblema2 (path)
+  (product (encontrar-tres-que-sumen *anioDelMal* (obtener-input path))))
