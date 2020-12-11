30e3bbbc75679d0e5115869238ca4b01e5a9c4ac
juanFdS <juan.fernandes@10pines.com>
2020-12-11 19:25:25 -0300

    WIP WIP WIP

--- dia-1/reportRepair.lisp
+++ dia-1/reportRepair.lisp
@@ -1,4 +1,4 @@
-(defparameter *lista* '())
+(defparameter *lista* (obtener-input "dia-1/input.txt"))
 
 (defun obtener-input (path)
   (mapcar #'parse-integer (uiop:read-file-lines path)))
@@ -11,11 +11,14 @@
   (find (complemento expensa) expensas))
 
 (defun tenemosSuComplementoDe (expensa total expensas)
-  (find-if (complemento-de expensa) expensas))
+  (find (complemento-de expensa total) expensas))
+
+(defun foo (complemento-de-la-expensa expensas)
+    (find-if (lambda (una-expensa) (tenemosSuComplementoDe una-expensa complemento-de-la-expensa expensas)) expensas))
 
 (defun tenemosSusComplementos (expensa expensas)
-  (let ((complemento (complemento expensa expensas)))
-    (find (complemento expensa) expensas)))
+  (let ((complemento-de-la-expensa (complemento expensa)))
+        (foo complemento-de-la-expensa expensas)))
 
 (defun producto-de-expensas-buscadas (expensas)
   (let ((expensa-buscada (find-if (lambda (expensa) (tenemosSuComplemento expensa expensas)) expensas)))
