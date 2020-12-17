fd3fc17d3e5ee99b6a8ccc45f0775adeac59ea96
juanFdS <juan.fernandes@10pines.com>
2020-12-17 16:35:03 -0300

    Primer test

--- dia-2/src/main.rs
+++ dia-2/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn contrasenia_valida_segun_politica(minimo: i32, maximo: i32, letra: char, contrasenia: String) -> bool {
+    return true;
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn foo() {
+        assert!(contrasenia_valida_segun_politica(1, 1, 'a', "a".to_string()));
+    }
+}
