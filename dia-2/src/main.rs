aed3e91784c96cec0de450b3b055e680ae141ad1
juanFdS <juan.fernandes@10pines.com>
2020-12-17 17:51:19 -0300

    Parser funciona

--- dia-2/src/main.rs
+++ dia-2/src/main.rs
@@ -5,8 +5,8 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn contrasenia_valida_segun_politica(minimo: i32, maximo: i32, letra: &str, contrasenia: String) -> bool {
-    let n = contrasenia.matches(letra).count() as i32;
+fn contrasenia_valida_segun_politica(minimo: i32, maximo: i32, letra: String, contrasenia: String) -> bool {
+    let n = contrasenia.matches(&letra).count() as i32;
     return n >= minimo && n <= maximo
 }
 
@@ -14,10 +14,11 @@ fn parsear_politica_y_contrasenia(input: String) -> (i32, i32, &'static str, Str
     return (1, 3, "a", "abcde".to_string());
 }
 
+#[derive(Debug, PartialEq)]
 struct ContraseniaYPolitica {
     minimo: i32,
     maximo: i32,
-    letra: &'static str,
+    letra: String,
     contrasenia: String
 }
 
@@ -31,7 +32,7 @@ impl FromStr for ContraseniaYPolitica {
         let rango: Vec<&str>= splited_input[0].split('-').collect();
         let minimo = rango[0].parse::<i32>()?;
         let maximo = rango[1].parse::<i32>()?;
-        let letra: &'static str = splited_input[1].chars().nth(0).unwrap().to_string();
+        let letra = splited_input[1].chars().nth(0).unwrap().to_string();
         let contrasenia = splited_input[2].to_string();
         Ok(ContraseniaYPolitica { minimo: minimo, maximo: maximo, letra: letra, contrasenia: contrasenia})
     }
@@ -44,39 +45,41 @@ mod tests {
 
     #[test]
     fn foo() {
-        assert!(contrasenia_valida_segun_politica(1, 1, "a", "a".to_string()));
+        assert!(contrasenia_valida_segun_politica(1, 1, "a".to_string(), "a".to_string()));
     }
 
     #[test]
     fn foo2() {
-        assert!(!contrasenia_valida_segun_politica(1, 1, "a", "b".to_string()));
+        assert!(!contrasenia_valida_segun_politica(1, 1, "a".to_string(), "b".to_string()));
     }
 
     #[test]
     fn contrasenia_no_es_valida_si_la_letra_pedida_aparece_menos_veces_que_el_minimo_de_la_politica() {
-        assert!(!contrasenia_valida_segun_politica(2, 3, "a", "a".to_string()));
+        assert!(!contrasenia_valida_segun_politica(2, 3, "a".to_string(), "a".to_string()));
     }
 
     #[test]
     fn contrasenia_no_es_valida_si_la_letra_pedida_aparece_mas_veces_que_el_maximo_de_la_politica() {
-        assert!(!contrasenia_valida_segun_politica(1, 1, "a", "aa".to_string()));
+        assert!(!contrasenia_valida_segun_politica(1, 1, "a".to_string(), "aa".to_string()));
     }
 
     #[test]
     fn bleh() {
-        assert!(contrasenia_valida_segun_politica(1, 3, "a", "abcde".to_string()));
-        assert!(!contrasenia_valida_segun_politica(1, 3, "b", "cdefg".to_string()));
-        assert!(contrasenia_valida_segun_politica(2, 9, "c", "ccccccccc".to_string()));
+        assert!(contrasenia_valida_segun_politica(1, 3, "a".to_string(), "abcde".to_string()));
+        assert!(!contrasenia_valida_segun_politica(1, 3, "b".to_string(), "cdefg".to_string()));
+        assert!(contrasenia_valida_segun_politica(2, 9, "c".to_string(), "ccccccccc".to_string()));
     }
 
     #[test]
     fn bleh2() {
-        assert_eq!(parsear_politica_y_contrasenia("1-3 a: abcde".to_string()), (1, 3, "a", "abcde".to_string()))
+        assert_eq!("1-3 a: abcde".parse::<ContraseniaYPolitica>(),
+                   Ok(ContraseniaYPolitica { minimo: 1, maximo: 3, letra: "a".to_string(), contrasenia: "abcde".to_string() }))
     }
 
     #[test]
     fn bleh3() {
-        assert_eq!(parsear_politica_y_contrasenia("1-3 b: cdefg".to_string()), (1, 3, "b", "cdefg".to_string()))
+        assert_eq!("1-3 b: cdefg".parse::<ContraseniaYPolitica>(),
+                    Ok(ContraseniaYPolitica { minimo: 1, maximo: 3, letra: "b".to_string(), contrasenia: "cdefg".to_string() }))
     }
 
 }
