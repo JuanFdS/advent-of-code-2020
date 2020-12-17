df774b155babed8e13ea0ab1f046d53661552047
agusgs <garciasmithagustin@gmail.com>
2020-12-17 16:40:38 -0300

    segundo test

--- dia-2/src/main.rs
+++ dia-2/src/main.rs
@@ -3,7 +3,7 @@ fn main() {
 }
 
 fn contrasenia_valida_segun_politica(minimo: i32, maximo: i32, letra: char, contrasenia: String) -> bool {
-    return true;
+    return contrasenia.contains(letra)
 }
 
 #[cfg(test)]
@@ -14,5 +14,10 @@ mod tests {
     #[test]
     fn foo() {
         assert!(contrasenia_valida_segun_politica(1, 1, 'a', "a".to_string()));
+    }
+
+    #[test]
+    fn foo2() {
+        assert!(!contrasenia_valida_segun_politica(1, 1, 'a', "b".to_string()));
     }
 }
